981ee499799f598cf90bb5f31022cf890f83212d
A253562 <A253562@LSBSA-9JJCSV3>
2023-12-05 10:48:14 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interactive_App
+{
+    internal class Program
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Enter your name");
+
+            string name = Console.ReadLine();
+            Console.WriteLine("Hi " + name);
+            Console.WriteLine("How old are you? ");
+            string age = Console.ReadLine();
+            Console.WriteLine("You are " + age + " old.");
+        }
+    }
+}
